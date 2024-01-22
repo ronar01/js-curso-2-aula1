049d6d9cf446ef3a941dddf94350e8102ba6a59f
ronar01 <105427300+ronar01@users.noreply.github.com>
2024-01-22 17:20:52 -0300

    Fiz o sistema de chutes e uma lista para prevenir numeros repetido

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,21 +1,53 @@
-preencherCampo('h1','Jogo do número secreto');
-preencherCampo('p', 'Escolha um número entre 1 e 10');
-
-let chute = 3;
-let numsecreto = parseInt((Math.random() * 10)) + 1;
+let listaSorteada = [];
+let tentativas = 1;
+let numeroLimite = 10;
+let numsecreto = gerarNumeroAleatorio();
+exibirMensagemInicial();
 
 function preencherCampo(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
-function verificarChute(){
-    if(chute == numsecreto){
-        paragrafo.innerHTML = 'Você acertou!';
-    }else if(chute > numsecreto){
-        paragrafo.innerHTML = 'É menor!';
-        console.log(numsecreto);
+function exibirMensagemInicial(){
+    preencherCampo('h1','Jogo do número secreto');
+    preencherCampo('p', 'Escolha um número entre 1 e 10');
+}
+function gerarNumeroAleatorio(){
+    if(listaSorteada.length == numeroLimite){
+        listaSorteada = [];
+    }
+    let numAleatorio = parseInt(Math.random() * numeroLimite + 1 );
+    if(listaSorteada.includes(numAleatorio)){
+        return gerarNumeroAleatorio();
     }else{
-        paragrafo.innerHTML = 'É maior!';
-        console.log(numsecreto);
+        listaSorteada.push(numAleatorio);
+        return numAleatorio;
+    }
+}
+function verificarChute(){
+    chute = document.querySelector('input').value;
+    if(chute == numsecreto){
+        let palavraTentativa = tentativas > 1 ? ' tentativas!' : ' tentativa!'
+        preencherCampo('p', 'Você ganhou! Com '+ tentativas + palavraTentativa);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+        if(chute > numsecreto){
+            preencherCampo('p', 'É menor!');
+        }else{
+            preencherCampo('p', 'É Maior!');
+        }
+        tentativas++;
+        limparCampo();
     }
 };
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+function reiniciarJogo(){
+    tentativas = 1;
+    numsecreto = gerarNumeroAleatorio();
+    exibirMensagemInicial();
+    limparCampo();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
